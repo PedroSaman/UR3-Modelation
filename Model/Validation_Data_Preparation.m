5788d93368a273ee269647eaa739308d33468db8
Pedro <pedrosaman96@gmail.com>
2019-09-11 11:30:03 -0300

    Data preparation and pre-calculations
    Those m files are called in Function_Model file for raw data preparation and pre-calculations

--- Model/Validation_Data_Preparation.m
+++ Model/Validation_Data_Preparation.m
@@ -1,11 +1,11 @@
-function [theta,d_theta,d_d_theta,torque,time] = Validation_Data_Preparation()
+function [theta,d_theta,d_d_theta,torque,time,omega,d_omega,alpha] = Validation_Data_Preparation()
     clear
     clc
     %dados_aleatorios.mat
     %dados_base.mat
     %dados_j5.mat
-    load ('~/Documents/Git/UR3-Modelation/MAT files/dados_j5.mat');
-
+    load ('~/Documents/Git/UR3-Modelation/MAT files/dados_aleatorios.mat');
+    
     time = dados(:,1);
     theta_J1 = dados(:,2);
     d_theta_J1 = dados(:,3);
@@ -48,4 +48,6 @@ function [theta,d_theta,d_d_theta,torque,time] = Validation_Data_Preparation()
     d_d_theta = [d_dtheta_J1 d_dtheta_J2 d_dtheta_J3 d_dtheta_J4 d_dtheta_J5 d_dtheta_J6];
     torque = [torque_J1 torque_J2 torque_J3 torque_J4 torque_J5 torque_J6]; 
     
+    %Pre_Calculations
+    [omega,d_omega,alpha] = Pre_Calculations(theta,d_theta,d_d_theta,time2);
 end
