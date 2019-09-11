701c1227065d5ba4fefc5c378ccca11d8031aef2
Pedro <pedrosaman96@gmail.com>
2019-09-11 20:43:17 -0300

    Minor fix
    Correcting vector length

--- Model/plotter.m
+++ Model/plotter.m
@@ -1,23 +1,23 @@
-plot(time,m_torque(:,1))
+plot(time(1:end-1),m_torque(1:end-1,1))
 hold on
-plot(time,torque(:,1))
+plot(time(1:end-1),torque(:,1))
 figure()
-plot(time,m_torque(:,2))
+plot(time(1:end-1),m_torque(1:end-1,2))
 hold on
-plot(time,torque(:,2))
+plot(time(1:end-1),torque(:,2))
 figure()
-plot(time,m_torque(:,3))
+plot(time(1:end-1),m_torque(1:end-1,3))
 hold on
-plot(time,torque(:,3))
+plot(time(1:end-1),torque(:,3))
 figure()
-plot(time,m_torque(:,4))
+plot(time(1:end-1),m_torque(1:end-1,4))
 hold on
-plot(time,torque(:,4))
+plot(time(1:end-1),torque(:,4))
 figure()
-plot(time,m_torque(:,5))
+plot(time(1:end-1),m_torque(1:end-1,5))
 hold on
-plot(time,torque(:,5))
+plot(time(1:end-1),torque(:,5))
 figure()
-plot(time,m_torque(:,6))
+plot(time(1:end-1),m_torque(1:end-1,6))
 hold on
-plot(time,torque(:,6))
+plot(time(1:end-1),torque(:,6))
