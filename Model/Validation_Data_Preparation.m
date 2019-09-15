bd437a17476b6d144bf1de7d7ff3e891c88ca4db
Pedro <pedrosaman96@gmail.com>
2019-09-15 13:04:15 -0300

    Variable names changed and organization improved

--- Model/Validation_Data_Preparation.m
+++ Model/Validation_Data_Preparation.m
@@ -1,10 +1,10 @@
-function [theta,d_theta,d_d_theta,torque,time,omega,d_omega,alpha] = Validation_Data_Preparation()
+function [q,d_q,d_d_q,torque,time,omega,d_omega,alpha] = Validation_Data_Preparation()
     clear
     clc
     %dados_aleatorios.mat
     %dados_base.mat
     %dados_j5.mat
-    load ('~/Documents/Git/UR3-Modelation/MAT files/dados_aleatorios.mat');
+    load ('~/Documents/Git/UR3-Modelation/MAT files/dados_base.mat');
     
     time = dados(:,1);
     theta_J1 = dados(:,2);
@@ -26,7 +26,7 @@ function [theta,d_theta,d_d_theta,torque,time,omega,d_omega,alpha] = Validation_
     d_theta_J6 = dados(:,18);
     torque_J6 = dados(:,19);
 
-    time2 = diff(time);
+    dt = diff(time);
     d_dtheta_J1 = diff(d_theta_J1);
     d_dtheta_J2 = diff(d_theta_J2);
     d_dtheta_J3 = diff(d_theta_J3);
@@ -35,19 +35,19 @@ function [theta,d_theta,d_d_theta,torque,time,omega,d_omega,alpha] = Validation_
     d_dtheta_J6 = diff(d_theta_J6);
 
     for i = 1:length(time) -1 
-        d_dtheta_J1(i) = d_dtheta_J1(i)/time2(i);
-        d_dtheta_J2(i) = d_dtheta_J2(i)/time2(i);
-        d_dtheta_J3(i) = d_dtheta_J3(i)/time2(i);
-        d_dtheta_J4(i) = d_dtheta_J4(i)/time2(i);
-        d_dtheta_J5(i) = d_dtheta_J5(i)/time2(i);
-        d_dtheta_J6(i) = d_dtheta_J6(i)/time2(i);
+        d_dtheta_J1(i) = d_dtheta_J1(i)/dt(i);
+        d_dtheta_J2(i) = d_dtheta_J2(i)/dt(i);
+        d_dtheta_J3(i) = d_dtheta_J3(i)/dt(i);
+        d_dtheta_J4(i) = d_dtheta_J4(i)/dt(i);
+        d_dtheta_J5(i) = d_dtheta_J5(i)/dt(i);
+        d_dtheta_J6(i) = d_dtheta_J6(i)/dt(i);
     end
 
-    theta = [theta_J1 theta_J2 theta_J3 theta_J4 theta_J5 theta_J6];
-    d_theta = [d_theta_J1 d_theta_J2 d_theta_J3 d_theta_J4 d_theta_J5 d_theta_J6];
-    d_d_theta = [d_dtheta_J1 d_dtheta_J2 d_dtheta_J3 d_dtheta_J4 d_dtheta_J5 d_dtheta_J6];
+    q = [theta_J1 theta_J2+pi/2 theta_J3 theta_J4+pi/2 theta_J5 theta_J6];
+    d_q = [d_theta_J1 d_theta_J2 d_theta_J3 d_theta_J4 d_theta_J5 d_theta_J6];
+    d_d_q = [d_dtheta_J1 d_dtheta_J2 d_dtheta_J3 d_dtheta_J4 d_dtheta_J5 d_dtheta_J6];
     torque = [torque_J1 torque_J2 torque_J3 torque_J4 torque_J5 torque_J6]; 
     
     %Pre_Calculations
-    [omega,d_omega,alpha] = Pre_Calculations(theta,d_theta,d_d_theta,time2);
+    [omega,d_omega,alpha] = Pre_Calculations(q,d_q,d_d_q,dt);
 end
