7d506fd1892fad3b1a40ee908ecbfba8f5325769
Pedro <pedrosaman96@gmail.com>
2020-08-28 16:59:35 -0300

    Proper Validation and Training data separation

--- Identification/Least Square Regression/Identification6DOF.m
+++ Identification/Least Square Regression/Identification6DOF.m
@@ -1,7 +1,7 @@
 function [theta1,theta2,theta3,theta4,theta5,theta6,FIT] = Identification6DOF()
 % MSQ identification    
-    clc 
-    [input] = Identification_data_Loader();
+    [input] = Identification_data_Loader(); %Id dados reais
+    %load 'simulated_data.mat' input; %Id de simulação
     [psi1,psi2,psi3,psi4,psi5,psi6] = PsiMatrix(input);
     
     l = length(input.tout);
@@ -14,65 +14,65 @@ function [theta1,theta2,theta3,theta4,theta5,theta6,FIT] = Identification6DOF()
     theta5 = pinv(psi5(1:0.7*l,:))*input.tau5(1:0.7*l);
     theta6 = pinv(psi6(1:0.7*l,:))*input.tau6(1:0.7*l);
     clc
-    
+
     %Validation section
-    etau1 = psi1(0.7*l+1:end,:)*theta1;
-    etau2 = psi2(0.7*l+1:end,:)*theta2;
-    etau3 = psi3(0.7*l+1:end,:)*theta3;
-    etau4 = psi4(0.7*l+1:end,:)*theta4;
-    etau5 = psi5(0.7*l+1:end,:)*theta5;
-    etau6 = psi6(0.7*l+1:end,:)*theta6;
+    etau1 = psi1(0.7*l:end,:)*theta1;
+    etau2 = psi2(0.7*l:end,:)*theta2;
+    etau3 = psi3(0.7*l:end,:)*theta3;
+    etau4 = psi4(0.7*l:end,:)*theta4;
+    etau5 = psi5(0.7*l:end,:)*theta5;
+    etau6 = psi6(0.7*l:end,:)*theta6;
     
     %Plotting
-    plot(input.tout(0.7*l+1:end,:),etau1);
+    plot(input.tout(0.7*l:end,:),etau1);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau1(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau1(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)') 
     figure
-    plot(input.tout(0.7*l+1:end,:),etau2);
+    plot(input.tout(0.7*l:end,:),etau2);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau2(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau2(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)')
     figure
-    plot(input.tout(0.7*l+1:end,:),etau3);
+    plot(input.tout(0.7*l:end,:),etau3);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau3(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau3(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)')
     figure
-    plot(input.tout(0.7*l+1:end,:),etau4);
+    plot(input.tout(0.7*l:end,:),etau4);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau4(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau4(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)')
     figure
-    plot(input.tout(0.7*l+1:end,:),etau5);
+    plot(input.tout(0.7*l:end,:),etau5);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau5(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau5(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)')
     figure
-    plot(input.tout(0.7*l+1:end,:),etau6);
+    plot(input.tout(0.7*l:end,:),etau6);
     hold on
-    plot(input.tout(0.7*l+1:end,:),input.tau6(0.7*l+1:end,:));
+    plot(input.tout(0.7*l:end,:),input.tau6(0.7*l:end,:));
     grid on
     legend({'Estimated Torque','Measured Torque'})
     xlabel('Time (s)') 
     ylabel('Torque (N.m)')
     
     %Curve Fitness
-    FIT = fitness([etau1,etau2,etau3,etau4,etau5,etau6],[input.tau1(0.7*l+1:end,:),input.tau2(0.7*l+1:end,:)...
-        ,input.tau3(0.7*l+1:end,:),input.tau4(0.7*l+1:end,:),input.tau5(0.7*l+1:end,:),input.tau6(0.7*l+1:end,:)])
+    FIT = fitness([etau1,etau2,etau3,etau4,etau5,etau6],[input.tau1(0.7*l:end,:),input.tau2(0.7*l:end,:)...
+        ,input.tau3(0.7*l:end,:),input.tau4(0.7*l:end,:),input.tau5(0.7*l:end,:),input.tau6(0.7*l:end,:)])
 end
